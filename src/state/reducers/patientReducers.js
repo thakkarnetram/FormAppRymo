3b8c77a34bc6186ac5fa98c62f87a92f06ad553f
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-07-07 22:00:04 +0530

    Patient reducer changed

--- src/state/reducers/patientReducers.js
+++ src/state/reducers/patientReducers.js
@@ -1,6 +1,10 @@
 const initialState = {
   patientList: [],
   patientName: '',
+  patientId: '',
+  patientAddress: '',
+  patientContact: '',
+  patientImage: '',
 };
 
 const reducer = (state = initialState, action) => {
@@ -9,6 +13,15 @@ const reducer = (state = initialState, action) => {
       return {...state, patientList: action.payload};
     case 'updatePatientName':
       return {...state, patientName: action.payload};
+    case 'updatePatientAddress':
+      return {...state, patientAddress: action.payload};
+    case 'updatePatientContact':
+      return {...state, patientContact: action.payload};
+    case 'updatePatientImageMain':
+      return {...state, patientImage: action.payload};
+    case 'updatePatientId':
+      return {...state, patientId: action.payload};
+
     default:
       return state;
   }
